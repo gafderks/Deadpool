33fe0916b04282828df935f425d392fb8775e0f9
Geert Derks <g.a.f.derks@student.tue.nl>
2017-06-01 14:18:26 +0200

    Attempt on nifty challenge 2017

---
+++ wbs_aes_ches2017/nifty/target/nifty.h
@@ -0,0 +1,14 @@
+#ifndef nifty_h
+#define nifty_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void AES_128_encrypt(unsigned char *out, unsigned char *in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
