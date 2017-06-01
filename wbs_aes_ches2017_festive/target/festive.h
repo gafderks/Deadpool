140eaca296dcbef532618ae6e38dfe123fd3e3d7
Amber van der Heijden <a.v.d.heijden@student.tue.nl>
2017-06-01 14:12:28 +0000

    Added wizardly, stupefied, practical, lucid, hopeful, focused, festive, elegant, angry, agitated

---
+++ wbs_aes_ches2017_festive/target/festive.h
@@ -0,0 +1,14 @@
+#ifndef festive_h
+#define festive_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void AES_128_encrypt(unsigned char *out, unsigned char *in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ wbs_aes_ches2017_practical/target/practical.h
@@ -0,0 +1,14 @@
+#ifndef practical_h
+#define practical_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void AES_128_encrypt(unsigned char *out, unsigned char *in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
