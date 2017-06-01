3461b35d82eca5e0abcf1765d8576cb9fcbd861d
Geert Derks <g.a.f.derks@student.tue.nl>
2017-06-01 15:56:02 +0200

    Added cranky, determined, friendly, modest, peaceful and quirky challenges from CHES2017

---
+++ wbs_aes_ches2017_modest/target/modest.h
@@ -0,0 +1,14 @@
+#ifndef modest_h
+#define modest_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void AES_128_encrypt(unsigned char *out, unsigned char *in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
