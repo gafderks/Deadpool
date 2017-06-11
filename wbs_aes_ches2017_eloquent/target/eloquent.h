162b05a14c28e5d99f792f917c763ba721bd9a81
Geert Derks <g.a.f.derks@student.tue.nl>
2017-06-11 21:25:45 +0200

    Compiled eloquent

---
+++ wbs_aes_ches2017_eloquent/target/eloquent.h
@@ -0,0 +1,14 @@
+#ifndef eloquent_h
+#define eloquent_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void AES_128_encrypt(unsigned char *out, unsigned char *in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
